1240ce27b1b4235c5ede06cfddcc1a40be57a157
beeMonkey <675383749@qq.com>
2018-08-24 14:33:27 +0800

    [Allen]add test for new release movie through rent one or two days

--- src/test/java/rentalstore/RentalTest.java
+++ src/test/java/rentalstore/RentalTest.java
@@ -44,4 +44,33 @@ public class RentalTest {
                 "Amount owed is 3.5\n" +
                 "You earned 1 frequent renter points", statement);
     }
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_new_release_movie_for_1_day() {
+        Movie newReleaseMovie = new Movie("Titanic", 1);
+        Rental oneDayRental = new Rental(newReleaseMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + newReleaseMovie.getTitle() + "\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+    
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_new_release_movie_for_2_day() {
+        Movie newReleaseMovie = new Movie("Titanic", 1);
+        Rental twoDayRental = new Rental(newReleaseMovie, 2);
+        customer.addRental(twoDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + newReleaseMovie.getTitle() + "\t6.0\n" +
+                "Amount owed is 6.0\n" +
+                "You earned 2 frequent renter points", statement);
+    }
+
 }
