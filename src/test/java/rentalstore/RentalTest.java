4b796173087bbfae72094b749c75324388014ee9
beeMonkey <675383749@qq.com>
2018-08-24 14:36:49 +0800

    [Allen]add test for child movie through rent one or four days

--- src/test/java/rentalstore/RentalTest.java
+++ src/test/java/rentalstore/RentalTest.java
@@ -58,7 +58,7 @@ public class RentalTest {
                 "Amount owed is 3.0\n" +
                 "You earned 1 frequent renter points", statement);
     }
-    
+
     @Test
     public void should_return_correct_statement_given_customer_has_rent_one_new_release_movie_for_2_day() {
         Movie newReleaseMovie = new Movie("Titanic", 1);
@@ -73,4 +73,31 @@ public class RentalTest {
                 "You earned 2 frequent renter points", statement);
     }
 
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_child_movie_for_1_day() {
+        Movie childrenMovie = new Movie("Titanic", 2);
+        Rental oneDayRental = new Rental(childrenMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + childrenMovie.getTitle() + "\t1.5\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_child_movie_for_4_day() {
+        Movie childrenMovie = new Movie("Titanic", 2);
+        Rental fourDayRental = new Rental(childrenMovie, 4);
+        customer.addRental(fourDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + childrenMovie.getTitle() + "\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
 }
