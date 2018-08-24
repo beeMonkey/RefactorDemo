c097cd081d8fa1162e06ed16ff83ca46246a8e7e
beeMonkey <675383749@qq.com>
2018-08-24 14:29:21 +0800

    [Allen]add test for rent regular movie,through one day or more days

--- src/test/java/rentalstore/RentalTest.java
+++ src/test/java/rentalstore/RentalTest.java
@@ -1,19 +1,47 @@
 package rentalstore;
 
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class RentalTest {
-    private Customer customer=new Customer("Allen");
+    private Customer customer=new Customer("Jerry");
 
     @Test
     public void should_return_correct_result_given_0_rental(){
         String statement=customer.statement();
 
-        assertEquals("Rental Record for Allen\n"
+        assertEquals("Rental Record for Jerry\n"
                         + "Amount owed is 0.0\n"
                         + "You earned 0 frequent renter points",
                 statement);
+    }
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_1_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental oneDayRental = new Rental(regularMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + regularMovie.getTitle() + "\t2.0\n" +
+                "Amount owed is 2.0\n" +
+                "You earned 1 frequent renter points", statement);
+    }
+
+    @Test
+    public void should_return_correct_statement_given_customer_has_rent_one_regular_movie_for_3_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental threeDayRental = new Rental(regularMovie, 3);
+        customer.addRental(threeDayRental);
+
+        String statement = customer.statement();
+
+        assertEquals("Rental Record for Jerry\n" +
+                "\t" + regularMovie.getTitle() + "\t3.5\n" +
+                "Amount owed is 3.5\n" +
+                "You earned 1 frequent renter points", statement);
     }
 }
