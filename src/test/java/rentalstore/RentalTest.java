feac9f9fa4d83502dae7590099a2839f6a7ddd29
beeMonkey <675383749@qq.com>
2018-08-26 13:40:51 +0800

    [Allen]add test for rent regular movie,through one day or more days,output is htmlStatement

--- src/test/java/rentalstore/RentalTest.java
+++ src/test/java/rentalstore/RentalTest.java
@@ -111,5 +111,49 @@ public class RentalTest {
                 statement);
     }
 
-    
+    @Test
+    public void should_return_correct_htmlstatement_given_customer_has_rent_one_regular_movie_for_1_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental oneDayRental = new Rental(regularMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.htmlStatement();
+
+        assertEquals("<H1>Rentals for <EM>Jerry</EM></H1><P>\n"
+                        + regularMovie.getTitle()+": 2.0<BR>\n"
+                        + "<P>You owe<EM>2.0</EM><P>\n"
+                        + "On this rental you earned <EM>1</EM> frequent renter points<P>",
+                        statement);
+    }
+
+    @Test
+    public void should_return_correct_htmlstatement_given_customer_has_rent_one_regular_movie_for_3_day() {
+        Movie regularMovie = new Movie("Titanic", 0);
+        Rental threeDayRental = new Rental(regularMovie, 3);
+        customer.addRental(threeDayRental);
+
+        String statement = customer.htmlStatement();
+
+        assertEquals("<H1>Rentals for <EM>Jerry</EM></H1><P>\n"
+                                + regularMovie.getTitle()+": 3.5<BR>\n"
+                                + "<P>You owe<EM>3.5</EM><P>\n"
+                                + "On this rental you earned <EM>1</EM> frequent renter points<P>",
+                                statement);
+    }
+
+    @Test
+    public void should_return_correct_html_statement_given_customer_has_rent_one_child_movie_for_4_day() {
+        Movie childrenMovie = new Movie("Titanic", 2);
+        Rental fourDayRental = new Rental(childrenMovie, 4);
+        customer.addRental(fourDayRental);
+
+        String statement = customer.htmlStatement();
+
+        System.out.println(statement);
+
+        assertEquals("<H1>Rentals for <EM>Jerry</EM></H1><P>\n" +
+                "Titanic: 3.0<BR>\n" +
+                "<P>You owe<EM>3.0</EM><P>\n" +
+                "On this rental you earned <EM>1</EM> frequent renter points<P>", statement);
+    }
 }
