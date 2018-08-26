066b3502681ad3254c1e53ef1bc12c94f70e6d57
beeMonkey <675383749@qq.com>
2018-08-26 13:52:01 +0800

    [Allen]add test for child movie through rent one or four days,output htmlstatement

--- src/test/java/rentalstore/RentalTest.java
+++ src/test/java/rentalstore/RentalTest.java
@@ -112,7 +112,7 @@ public class RentalTest {
     }
 
     @Test
-    public void should_return_correct_htmlstatement_given_customer_has_rent_one_regular_movie_for_1_day() {
+    public void should_return_correct_html_statement_given_customer_has_rent_one_regular_movie_for_1_day() {
         Movie regularMovie = new Movie("Titanic", 0);
         Rental oneDayRental = new Rental(regularMovie, 1);
         customer.addRental(oneDayRental);
@@ -127,7 +127,7 @@ public class RentalTest {
     }
 
     @Test
-    public void should_return_correct_htmlstatement_given_customer_has_rent_one_regular_movie_for_3_day() {
+    public void should_return_correct_html_statement_given_customer_has_rent_one_regular_movie_for_3_day() {
         Movie regularMovie = new Movie("Titanic", 0);
         Rental threeDayRental = new Rental(regularMovie, 3);
         customer.addRental(threeDayRental);
@@ -142,7 +142,7 @@ public class RentalTest {
     }
 
     @Test
-    public void should_return_correct_htmlstatement_given_customer_has_rent_one_new_release_movie_for_1_day() {
+    public void should_return_correct_html_statement_given_customer_has_rent_one_new_release_movie_for_1_day() {
         Movie newReleaseMovie = new Movie("Titanic", 1);
         Rental oneDayRental = new Rental(newReleaseMovie, 1);
         customer.addRental(oneDayRental);
@@ -157,7 +157,7 @@ public class RentalTest {
     }
 
     @Test
-    public void should_return_correct_htmlstatement_given_customer_has_rent_one_new_release_movie_for_2_day() {
+    public void should_return_correct_html_statement_given_customer_has_rent_one_new_release_movie_for_2_day() {
         Movie newReleaseMovie = new Movie("Titanic", 1);
         Rental twoDayRental = new Rental(newReleaseMovie, 2);
         customer.addRental(twoDayRental);
@@ -168,6 +168,21 @@ public class RentalTest {
                 newReleaseMovie.getTitle()+": 6.0<BR>\n" +
                 "<P>You owe<EM>6.0</EM><P>\n" +
                 "On this rental you earned <EM>2</EM> frequent renter points<P>",
+                statement);
+    }
+
+    @Test
+    public void should_return_correct_html_statement_given_customer_has_rent_one_child_movie_for_1_day() {
+        Movie childrenMovie = new Movie("Titanic", 2);
+        Rental oneDayRental = new Rental(childrenMovie, 1);
+        customer.addRental(oneDayRental);
+
+        String statement = customer.htmlStatement();
+
+        assertEquals("<H1>Rentals for <EM>Jerry</EM></H1><P>\n" +
+                        childrenMovie.getTitle()+": 1.5<BR>\n" +
+                        "<P>You owe<EM>1.5</EM><P>\n" +
+                        "On this rental you earned <EM>1</EM> frequent renter points<P>",
                 statement);
     }
 
